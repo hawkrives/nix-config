37b669fb263ac58f23f960c1d7b0d84f56def43b
Hawken Rives <hawkrives@fastmail.fm>
2025-06-07 15:00:58 -0400

    remove brew things I no longer use

--- hosts/Techcyte-DGQJV434PF/configuration.nix
+++ hosts/Techcyte-DGQJV434PF/configuration.nix
@@ -68,42 +68,32 @@
     casks = [
       "font-blex-mono-nerd-font"
       "anytype"
-      "arc"
       "bbedit"
       "bike"
       "docker"
       "firefox"
-      "firefox@nightly"
       "gitup"
       "google-chrome"
       "handbrake"
-      "iterm2"
       "jetbrains-toolbox"
       "kaleidoscope"
       "mimestream"
       "mullvadvpn"
       "nova"
       "plexamp"
-      "remote-desktop-manager"
       "sublime-merge"
       "sublime-text"
-      "telegram-desktop"
-      "thunderbird"
       "visual-studio-code"
       "vlc"
-      "wezterm"
-      "yattee"
       "zed"
     ];
 
     masApps = {
       "Acorn 7" = 1547371478;
       "AWS Extend Switch Roles" = 1592710340;
-      # "Bike Outliner" = 1588292384;
       "iA Writer" = 775737590;
       "Keynote" = 409183694;
       "Lungo" = 1263070803; # caffeine
-      # "Notchmeister" = 1599169747;
       "Numbers" = 409203825;
       "Pages" = 409201541;
       "Paste" = 967805235;
