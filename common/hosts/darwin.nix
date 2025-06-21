da2c9eecf0f90483f6f9d053ba0ba8464b47a4a3
Hawken Rives <hawkrives@fastmail.fm>
2025-06-21 14:36:36 -0400

    fix key conflict

--- common/hosts/darwin.nix
+++ common/hosts/darwin.nix
@@ -19,6 +19,4 @@
   # Used for backwards compatibility, please read the changelog before changing.
   # $ darwin-rebuild changelog
   system.stateVersion = 4;
-
-  system.defaults.dock.autohide = true;
 }
