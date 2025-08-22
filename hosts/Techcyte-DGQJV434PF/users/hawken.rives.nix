b65c4311f6ff2a9e1fd32c4248432a1962dde4cc
Hawken Rives <hawkrives@fastmail.fm>
2025-08-21 22:29:53 -0400

    try a few things on nutmeg

--- hosts/Techcyte-DGQJV434PF/users/hawken.rives.nix
+++ hosts/Techcyte-DGQJV434PF/users/hawken.rives.nix
@@ -1,8 +1,16 @@
-{inputs, ...}: {
+{
+  inputs,
+  pkgs,
+  ...
+}: {
   imports = [
     inputs.self.homeModules.home-shared
     inputs.self.homeModules.git-shared
     inputs.self.homeModules.helix-shared
     inputs.self.homeModules.sqlite-shared
+  ];
+
+  home.packages = [
+    pkgs.awscli2
   ];
 }
