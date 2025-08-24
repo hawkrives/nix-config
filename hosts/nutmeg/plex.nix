19fedd825058d2c183c58e5ff3865cf1cb660c32
Hawken Rives <hawkrives@fastmail.fm>
2025-08-24 14:03:56 -0400

    reboot plex every 4 hours, because it silently becomes unresponsive since 1.42

--- hosts/nutmeg/plex.nix
+++ hosts/nutmeg/plex.nix
@@ -1,4 +1,4 @@
-{pkgs, ...}: let
+{pkgs, lib, ...}: let
   nfsOptions = [
     "nfsvers=4.1"
     "noatime" # we do not care about tracking the access time
@@ -37,6 +37,10 @@ in {
       })
     ];
   };
+
+  # HACK: restart plex evey 4 hours
+  systemd.services.plex.serviceConfig.RuntimeMaxSec = "4h";
+  systemd.services.plex.serviceConfig.Restart = lib.mkForce "always";
 
   services.tautulli = {
     enable = true;
