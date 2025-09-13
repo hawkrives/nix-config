eb208dadc2276c6d1616ddb197996297f84706ff
Hawken Rives <hawkrives@fastmail.fm>
2025-09-13 06:40:24 -0400

    syncthing tweakso

--- hosts/nutmeg/syncthing.nix
+++ hosts/nutmeg/syncthing.nix
@@ -6,15 +6,13 @@
 
     settings = {
       devices = {
-        long-blippp = {
-          id = "BVVVV2C-BN26FQM-OI4GTM5-MSUCFJK-6FDTOJG-MWYO2K4-EFBDOL2-VZG4IQB";
-          # autoAcceptFolders = true;
-        };
+        long-blippp = {id = "BVVVV2C-BN26FQM-OI4GTM5-MSUCFJK-6FDTOJG-MWYO2K4-EFBDOL2-VZG4IQB";};
+        techcyte-DGQJV434PF = {id = "E7TASFG-GOULRTE-VOLVZTN-DOA4BA2-DFBSYR5-KZURLEU-XJAZDKK-QSEQWAL";};
       };
 
       folders = {
-        "~/paperless" = {devices = ["long-blippp"];};
-        "~/icloud" = {devices = ["long-blippp"];};
+        "~/paperless" = {devices = ["long-blippp" "techcyte-DGQJV434PF"];};
+        "~/icloud" = {devices = ["long-blippp" "techcyte-DGQJV434PF"];};
       };
     };
   };
