9c78eab6ecd12d2bd3bc6f9f010e15efa86e2639
Hawken Rives <hawkrives@fastmail.fm>
2025-09-25 20:58:36 -0400

    add new helix keybinds

--- modules/home/helix-shared.nix
+++ modules/home/helix-shared.nix
@@ -40,11 +40,46 @@ in {
         hidden = false;
       };
 
+      editor.soft-wrap.enable = true;
+
+      editor.end-of-line-diagnostics = "hint";
+      editor.inline-diagnostics.cursor-line = "error";
+
       keys = {
         normal = {
           # make D behave like vim
           # NOTE: t⏎ will select to line end!
           D = ["kill_to_line_end"];
+
+          # from https://github.com/helix-editor/helix/pull/9080#issuecomment-2008964831
+          J = "select_line_below";
+          K = "select_line_above";
+
+          "A-j" = "join_selections";
+          "A-J" = "join_selections_space";
+
+          X = "remove_primary_selection";
+          x = "keep_selections";
+          "A-x" = "remove_selections";
+
+          "H" = ["select_mode" "goto_line_start" "exit_select_mode"];
+          "L" = ["select_mode" "goto_line_end" "exit_select_mode"];
+        };
+
+        select = {
+          # from https://github.com/helix-editor/helix/pull/9080#issuecomment-2008964831
+          J = "select_line_below";
+          K = "select_line_above";
+
+          "A-j" = "join_selections";
+          "A-J" = "join_selections_space";
+
+          X = "remove_primary_selection";
+          x = "keep_selections";
+          "A-x" = "remove_selections";
+
+          "H" = "goto_line_start";
+          "L" = "goto_line_end";
         };
       };
     };
