65fd36ac9b6785cb5e2acd382502c7e8f17a37a2
Hawken Rives <hawkrives@fastmail.fm>
2025-10-06 00:02:07 -0400

    more discourse settings

--- hosts/nutmeg/discourse.nix
+++ hosts/nutmeg/discourse.nix
@@ -7,5 +7,11 @@
     siteSettings = {
       email.disable_emails = "yes";
     };
+    admin = {
+      email = "admin@nutmeg.local";
+      fullName = "Admin";
+      passwordFile = "/etc/discourse/admin-password.txt";
+      username = "admin";
+    };
   };
 }
