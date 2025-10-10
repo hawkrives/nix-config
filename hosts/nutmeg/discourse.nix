f489db1eae2e2c1ae89d6a626b091105e778de27
Hawken Rives <hawkrives@fastmail.fm>
2025-10-10 02:38:23 -0400

    increase rate limits for discourse

--- hosts/nutmeg/discourse.nix
+++ hosts/nutmeg/discourse.nix
@@ -4,7 +4,24 @@
     enable = true;
     hostname = "nutmeg.local";
     enableACME = false;
-    backendSettings = {};
+    backendSettings = {
+      max_reqs_per_ip_per_minute = 30000;
+      max_reqs_per_ip_per_10_seconds = 6000;
+      max_asset_reqs_per_ip_per_10_seconds = 25000;
+
+      ### rate limits apply to all sites
+      max_user_api_reqs_per_minute = 2000;
+      max_user_api_reqs_per_day = 288000;
+      max_admin_api_reqs_per_key_per_minute = 60000;
+      # max_reqs_per_ip_per_minute = 200;
+      # max_reqs_per_ip_per_10_seconds = 50;
+      # applies to asset type routes (avatars/css and so on)
+      # max_asset_reqs_per_ip_per_10_seconds = 200;
+      # global rate limiter will simply warn if the limit is exceeded, can be warn+block, warn, block or none
+      max_reqs_per_ip_mode = "warn";
+      # bypass rate limiting any IP resolved as a private IP
+      max_reqs_rate_limit_on_private = false;
+    };
     siteSettings = {
       email.disable_emails = "yes";
     };
