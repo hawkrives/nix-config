975fe1c8d02b95cc0f806a692b194fd404a23c6f
Hawken Rives <hawkrives@fastmail.fm>
2025-11-02 03:50:40 -0500

    [nutmeg] some cleanup in the tailscale config

--- hosts/nutmeg/tailscale.nix
+++ hosts/nutmeg/tailscale.nix
@@ -1,17 +1,14 @@
-{...}: {
+{config, ...}: {
   services.tailscale = {
     enable = true;
     openFirewall = true; # allow the Tailscale UDP port through the firewall
     useRoutingFeatures = "both";
     permitCertUid = "nginx";
-    extraUpFlags = [
-      "--accept-dns=false"
-      "--advertise-exit-node"
-    ];
+    extraUpFlags = ["--accept-dns=false" "--advertise-exit-node"];
   };
 
   # always allow traffic from your Tailscale network
-  networking.firewall.trustedInterfaces = ["tailscale0"];
+  networking.firewall.trustedInterfaces = [config.services.tailscale.interfaceName];
 
   environment.systemPackages = [
     # tsnsrv.default
