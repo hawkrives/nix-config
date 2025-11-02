b9700fbcac3b5a6f1b99b412d0041338d55760fb
Hawken Rives <hawkrives@fastmail.fm>
2025-11-01 22:03:16 -0400

    fix system.configurationRevision on darwin

--- modules/darwin/host-shared.nix
+++ modules/darwin/host-shared.nix
@@ -1,7 +1,4 @@
-{
-  # self, # TODO might need to remove this
-  ...
-}: {
+{inputs, ...}: {
   # enable dragging windows from anywhere anywhere while holding the control and command keys
   system.defaults.NSGlobalDomain.NSWindowShouldDragOnGesture = true;
 
@@ -9,9 +6,9 @@
   security.pam.services.sudo_local.touchIdAuth = true;
 
   # Set Git commit hash for darwin-version.
-  # system.configurationRevision = self.rev or self.dirtyRev or null;
 
   # on mac, please give me an updated version of bash
   # because I do still need bash for things
   programs.bash.enable = true;
+  system.configurationRevision = inputs.self.rev or inputs.self.dirtyRev or null;
 }
