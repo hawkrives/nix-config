3a4b968f16369325d07fdc61ac0ebb5d004a0ad9
Hawken Rives <hawkrives@fastmail.fm>
2025-11-02 03:50:40 -0500

    attempt to config tailscale services

--- hosts/nutmeg/tailscale.nix
+++ hosts/nutmeg/tailscale.nix
@@ -1,6 +1,11 @@
-{config, ...}: {
+{
+  config,
+  perSystem,
+  ...
+}: {
   services.tailscale = {
     enable = true;
+    package = perSystem.nixpkgs-unstable.tailscale;
     openFirewall = true; # allow the Tailscale UDP port through the firewall
     useRoutingFeatures = "both";
     permitCertUid = "nginx";
@@ -10,7 +15,17 @@
   # always allow traffic from your Tailscale network
   networking.firewall.trustedInterfaces = [config.services.tailscale.interfaceName];
 
-  environment.systemPackages = [
-    # tsnsrv.default
-  ];
+  environment.etc."tailscale-serve.json" = builtins.toJSON {
+    version = "0.0.1"; # The Service configuration file format version. Must be "0.0.1".
+    services = {
+      # "svc:<service-name>" is the name of the service (for example, `svc:web-server` or `svc:printer`)
+      "svc:adguard" = {
+        # Can contain one or more endpoint mappings
+        "endpoints" = {
+          # "<protocol>:<port|port-range>" = "<local-target>"; # Maps incoming traffic to a local target
+          "tcp:443" = "http://localhost:5380";
+        };
+      };
+    };
+  };
 }
