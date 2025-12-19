0f1b523eba33a66465b50f5248e7ef0e76ec6aa9
Hawken Rives <hawkrives@fastmail.fm>
2025-12-18 19:53:24 -0500

    enable new nix-darwin 25.11 options

--- modules/darwin/host-shared.nix
+++ modules/darwin/host-shared.nix
@@ -2,6 +2,13 @@
   # enable dragging windows from anywhere anywhere while holding the control and command keys
   system.defaults.NSGlobalDomain.NSWindowShouldDragOnGesture = true;
 
+  # reduce spacing around menu bar items
+  # system.defaults.NSGlobalDomain.NSStatusItemSelectionPadding = 6;
+  # system.defaults.NSGlobalDomain.NSStatusItemSpacing = 12;
+
+  # enable column auto-resize in Findeer
+  # system.defaults.finder._FXEnableColumnAutoSizing = true;
+
   # Enable sudo authentication with Touch ID
   security.pam.services.sudo_local.touchIdAuth = true;
 
