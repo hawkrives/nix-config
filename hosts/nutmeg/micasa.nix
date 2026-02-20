453cd98d8cf854b02753150c6a8599f490f35cb1
Hawken Rives <hawkrives@fastmail.fm>
2026-02-20 10:54:26 -0500

    fix micasa setup

--- hosts/nutmeg/micasa.nix
+++ hosts/nutmeg/micasa.nix
@@ -1,7 +1,11 @@
-{perSystem, ...}: {
+{perSystem, inputs, ...}: {
+  imports = [
+    inputs.micasa.nixosModules.default
+  ];
+
   services.micasa = {
     enable = true;
-    package = perSystem.micasa.packages.default;
+    package = perSystem.micasa.default;
     # if we want to add keys explicitly for micasa; this merges with the openssh setting
     # authorizedKeys = [ ... ];
   };
