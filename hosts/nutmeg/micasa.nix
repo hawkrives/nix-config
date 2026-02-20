16beffd921cd1277512c684244c8c69d6226d91a
Hawken Rives <hawkrives@fastmail.fm>
2026-02-20 10:59:30 -0500

    [nutmeg] disable micasa until the module is fixed

--- hosts/nutmeg/micasa.nix
+++ hosts/nutmeg/micasa.nix
@@ -4,7 +4,8 @@
   ];
 
   services.micasa = {
-    enable = true;
+    enable = false; # until the nixos module is fixed
+    # enable = true;
     package = perSystem.micasa.default;
     # if we want to add keys explicitly for micasa; this merges with the openssh setting
     # authorizedKeys = [ ... ];
