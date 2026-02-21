9e7b22fe665b6b1ca8daf58f7b3879a88b54ef1c
Hawken Rives <hawkrives@fastmail.fm>
2026-02-21 01:25:21 -0500

    Revert "[nutmeg] disable micasa until the module is fixed"
    This reverts commit 16beffd921cd1277512c684244c8c69d6226d91a.

--- hosts/nutmeg/micasa.nix
+++ hosts/nutmeg/micasa.nix
@@ -4,8 +4,7 @@
   ];
 
   services.micasa = {
-    enable = false; # until the nixos module is fixed
-    # enable = true;
+    enable = true;
     package = perSystem.micasa.default;
     # if we want to add keys explicitly for micasa; this merges with the openssh setting
     # authorizedKeys = [ ... ];
