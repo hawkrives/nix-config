35250589c17b7cc9980aa499659634960ea9719a
Hawken Rives <hawkrives@fastmail.fm>
2026-02-23 15:03:04 -0500

    add authorized key for micasa

--- hosts/nutmeg/micasa.nix
+++ hosts/nutmeg/micasa.nix
@@ -6,7 +6,8 @@
   services.micasa = {
     enable = true;
     package = perSystem.micasa.default;
-    # if we want to add keys explicitly for micasa; this merges with the openssh setting
-    # authorizedKeys = [ ... ];
+    authorizedKeys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILFhbHFf1LJ/NseB3yDEAKNu3CGNDs+ot8qdQA5LI4rU hawken.rives@Techcyte-DGQJV434PF"
+    ];
   };
 }
