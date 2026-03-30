fa288f4529215d5146fdb37f3b76e5ba6d76bba5
Hawken Rives <hawkrives@fastmail.fm>
2026-03-30 01:36:42 -0400

    expose adguard via tsnsrv

--- hosts/nutmeg/adguard.nix
+++ hosts/nutmeg/adguard.nix
@@ -1,16 +1,18 @@
-{...}: let
-  port = 5380;
-in {
+{config, ...}: {
   # it's OK to open :53, because the router won't allow unexpected connections
   # from the internet to the machines
-  networking.firewall.allowedTCPPorts = [53 port];
-  networking.firewall.allowedUDPPorts = [53 port];
+  networking.firewall.allowedTCPPorts = [53];
+  networking.firewall.allowedUDPPorts = [53];
 
   services.adguardhome = {
     enable = true;
     mutableSettings = true;
-    port = port;
+    openFirewall = true;
+    port = 5380;
+
     # settings.http.address = "0.0.0.0:${toString port}";
     settings.dns.bind_hosts = ["0.0.0.0"];
   };
+
+  services.tsnsrv.services.adguard.toURL = "http://localhost:${toString config.services.adguardhome.port}";
 }
