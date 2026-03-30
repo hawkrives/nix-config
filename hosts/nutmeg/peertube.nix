83de8a15f72a09ef0aa7e3f399309a9551f41012
Hawken Rives <hawkrives@fastmail.fm>
2026-03-30 14:09:42 -0400

    enable peertube more

--- hosts/nutmeg/peertube.nix
+++ hosts/nutmeg/peertube.nix
@@ -1,4 +1,5 @@
 {config, ...}: {
+  services.tsnsrv.services.peertube.urlParts.host = "127.0.0.1";
   services.tsnsrv.services.peertube.urlParts.port = config.services.peertube.listenHttp;
 
   services.peertube = {
@@ -6,15 +7,28 @@
     listenWeb = 443;
     # configureNginx = true;
     localDomain = "peertube.vaquita-woodpecker.ts.net";
+    enableWebHttps = true;
 
     redis.createLocally = true;
     database.createLocally = true;
 
     secrets.secretsFile = "/etc/peertube/secret";
 
+    # # enable once we have soops/agenix set up
+    # secrets.smtpPasswordFile = "/run/secrets/peertube-smtp-password";
+    # smtp = {
+    #   createLocally = false;
+    #   hostname = "smtp.fastmail.com";
+    #   port = 465;
+    #   from = "your-address@fastmail.com";
+    #   username = "your-address@fastmail.com";
+    #   tls = true;
+    # };
+
     settings = {
-      instance.name = "PeerTube Test Server";
-      video_transcription.enabled = true;
+      instance.name = "vidz-bop";
+      # whisper is broken (2026-03-30) due to a python dependency on 26.05
+      # video_transcription.enabled = true;
     };
   };
 
