f6de5487472ab7d55f13201863bf9cee24d104e8
Hawken Rives <hawkrives@fastmail.fm>
2026-06-13 19:05:39 -0400

    remove duplicated(?) nixpkgs.overlays rule

--- modules/nixos/host-shared.nix
+++ modules/nixos/host-shared.nix
@@ -36,19 +36,6 @@
       pkgs.nh
     ]);
 
-  nixpkgs.overlays = [
-    (final: prev: {
-      inherit
-        (final.lixPackageSets.stable)
-        nixpkgs-review
-        nix-direnv
-        nix-eval-jobs
-        nix-fast-build
-        colmena
-        ;
-    })
-  ];
-
   nix.package = pkgs.lixPackageSets.stable.lix;
 
   # TODO: document
