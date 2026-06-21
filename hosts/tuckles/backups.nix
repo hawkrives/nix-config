d1d52a68d85e21df2a648731d444bc4151e43042
Hawken Rives <hawkrives@fastmail.fm>
2026-06-20 22:29:28 -0400

    tuckles: daily config-dir backup to NAS

--- hosts/tuckles/backups.nix
+++ hosts/tuckles/backups.nix
@@ -1,3 +1,25 @@
-{ ... }:
+{ pkgs, synologyMount, ... }:
 {
+  # Mount the app-servarr share read-write for backups.
+  fileSystems."/mnt/servarr" = synologyMount "/volume1/app-servarr" { };
+
+  systemd.services.download-client-config-backup = {
+    description = "Back up SAB/qB config dirs to the NAS";
+    path = [ pkgs.rsync ];
+    serviceConfig.Type = "oneshot";
+    script = ''
+      dest=/mnt/servarr/backups/tuckles
+      mkdir -p "$dest"
+      rsync -a --delete /var/lib/sabnzbd/ "$dest/sabnzbd/"
+      rsync -a --delete /var/lib/qbittorrent/ "$dest/qbittorrent/"
+    '';
+  };
+
+  systemd.timers.download-client-config-backup = {
+    wantedBy = [ "timers.target" ];
+    timerConfig = {
+      OnCalendar = "daily";
+      Persistent = true;
+    };
+  };
 }
