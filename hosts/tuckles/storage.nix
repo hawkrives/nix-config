770f35d9d524b8903f9007a8160642d8ec6ce789
Hawken Rives <hawkrives@fastmail.fm>
2026-06-20 22:28:30 -0400

    tuckles: NFS mounts for media shares

--- hosts/tuckles/storage.nix
+++ hosts/tuckles/storage.nix
@@ -1,3 +1,6 @@
-{ ... }:
+{ synologyMount, ... }:
 {
+  fileSystems."/mnt/music" = synologyMount "/volume1/media-music" { };
+  fileSystems."/mnt/movies" = synologyMount "/volume1/media-movies" { };
+  fileSystems."/mnt/shows" = synologyMount "/volume1/media-shows" { };
 }
