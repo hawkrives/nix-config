0d7d59abc7e14da20d362422dc02d8c9dbc8d082
Hawken Rives <hawkrives@fastmail.fm>
2026-06-20 22:28:47 -0400

    tuckles: SABnzbd service

--- hosts/tuckles/sabnzbd.nix
+++ hosts/tuckles/sabnzbd.nix
@@ -1,3 +1,16 @@
 { ... }:
 {
+  services.sabnzbd = {
+    enable = true;
+    # configFile defaults to /var/lib/sabnzbd/sabnzbd.ini; the runtime ini is
+    # seeded once from the migrated old config (not managed declaratively).
+  };
+
+  # SAB listens on 6000 (set in the seeded ini). Open it on the LAN.
+  networking.firewall.allowedTCPPorts = [ 6000 ];
+
+  systemd.tmpfiles.rules = [
+    "d /var/lib/sabnzbd/incomplete 0770 sabnzbd sabnzbd -"
+    "d /var/lib/sabnzbd/complete 0770 sabnzbd sabnzbd -"
+  ];
 }
