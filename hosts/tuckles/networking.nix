9883a9e072d01bd5a62ae12b7d00a5537e341539
Hawken Rives <hawkrives@fastmail.fm>
2026-06-20 22:28:18 -0400

    tuckles: static LAN IP and tailscale

--- hosts/tuckles/networking.nix
+++ hosts/tuckles/networking.nix
@@ -1,3 +1,19 @@
-{ ... }:
+{ config, ... }:
 {
+  # Static LAN address via systemd-networkd (configuration.nix sets useNetworkd).
+  systemd.network.networks."10-lan" = {
+    matchConfig.Name = "en*";
+    address = [ "192.168.1.195/24" ];
+    routes = [ { Gateway = "192.168.1.1"; } ];
+    networkConfig.DNS = "192.168.1.194";
+    linkConfig.RequiredForOnline = "routable";
+  };
+
+  # Tailscale (LAN + tailnet).
+  services.tailscale = {
+    enable = true;
+    openFirewall = true;
+    authKeyFile = config.age.secrets.tailscale-authkey-tuckles.path;
+  };
+  networking.firewall.trustedInterfaces = [ config.services.tailscale.interfaceName ];
 }
