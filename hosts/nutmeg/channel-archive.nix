8071ff44c6d8e1975fce03d3f55fb5d0bcb1c28e
Hawken Rives <hawkrives@fastmail.fm>
2026-07-30 01:21:59 -0400

    nutmeg: add 6 pinchflat YouTube channels to channelArchive

--- hosts/nutmeg/channel-archive.nix
+++ hosts/nutmeg/channel-archive.nix
@@ -8,5 +8,35 @@
     channels.ditherdown = {
       url = "https://www.twitch.tv/ditherdown/videos?filter=archives&sort=time";
     };
+    channels."settei-seven" = {
+      url = "https://www.youtube.com/channel/UCedsCHD4XKPg5YiK56jTypg/videos";
+      destination = "/mnt/channels/Settei Seven";
+      rateLimit = true;
+    };
+    channels."axell-the-swampert" = {
+      url = "https://www.youtube.com/channel/UCzd7kTq1TCKOA6kPWaW2Z8Q/videos";
+      destination = "/mnt/channels/Axell The Swampert";
+      rateLimit = true;
+    };
+    channels."jen" = {
+      url = "https://www.youtube.com/channel/UCjQSQVa1-OgDmp4ypfdRbWQ/videos";
+      destination = "/mnt/channels/jen";
+      rateLimit = true;
+    };
+    channels."adrisaurus" = {
+      url = "https://www.youtube.com/channel/UCAHPCNxU4A-TUV-lnu7u4tA/videos";
+      destination = "/mnt/channels/adrisaurus";
+      rateLimit = true;
+    };
+    channels."displaced-gamers" = {
+      url = "https://www.youtube.com/channel/UCWoSKWs8h6lFdiEDAjuIfpA/videos";
+      destination = "/mnt/channels/Displaced Gamers";
+      rateLimit = true;
+    };
+    channels."caitlin-myers" = {
+      url = "https://www.youtube.com/channel/UCDzdRLILLWlHxXnj3HhXW0A/videos";
+      destination = "/mnt/channels/Caitlin Myers";
+      rateLimit = true;
+    };
   };
 }
