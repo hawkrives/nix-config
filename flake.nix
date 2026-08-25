28fd3663c3c98985d381fb975228146236cb76a8
Hawken Rives <hawkrives@fastmail.fm>
2026-08-25 14:25:53 -0400

    fix pin for rust-overlay to avoid rebuilding ragenix on any minor patch update to rust

--- flake.nix
+++ flake.nix
@@ -42,13 +42,18 @@
       inputs.flake-utils.inputs.systems.follows = "systems";
     };
 
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     ragenix = {
       url = "github:yaxitech/ragenix";
       inputs.nixpkgs.follows = "nixpkgs";
       # ragenix pins rust-overlay to Oct 2025, which breaks eval against
       # current nixpkgs (stdenv.isLinux/.isDarwin removal). Track a fresh
       # rust-overlay ourselves until upstream ragenix catches up.
-      inputs.rust-overlay.url = "github:oxalica/rust-overlay";
+      inputs.rust-overlay.follows = "rust-overlay";
     };
 
     disko = {
